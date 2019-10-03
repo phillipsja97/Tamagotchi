4755f322fcab3d5a2446452de276678d9d530440
Jamie <phillipsja97@gmail.com>
2019-10-02 20:53:05 -0500

    buttons added and eat functioning

--- src/javascripts/components/fight.js
+++ src/javascripts/components/fight.js
@@ -6,6 +6,14 @@ const printFight = () => {
   <div class="fightContainer">
   <h2>Fight</h2>
   </div>
+  <div class="overallButton">
+  <button>Run Away From the Fight</button>
+  <button>Activate: Violence</button>
+  </div>
+  <label>Progress</label>
+  <div class="checkProgress">
+  <progress max="100" value="70"> 70% </progress>
+  </div>
   `;
   utilities.printToDom('fight', domString);
 };
