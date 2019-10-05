62094fc4fd4df77a2900938623c000a87720b9ea
Jamie <phillipsja97@gmail.com>
2019-10-05 13:49:01 -0500

    added image and centered on page

--- src/javascripts/components/tamagotchiImage.js
+++ src/javascripts/components/tamagotchiImage.js
@@ -1,10 +1,8 @@
 import utilities from '../helpers/utilities';
-import gucci from '../../assets/gucci.jpg';
-
 
 const printImage = () => {
   const domString = `
-  <img src='<img src=${gucci} alt="picture of gucci"/>'/>
+  <img src="http://assets.stickpng.com/thumbs/5a2d8c0ab3c4622cbe35976e.png" alt="picture of gucci"/>
   `;
   utilities.printToDom('pet', domString);
 };
